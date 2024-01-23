47c9d49b513b4a92a596d24cf8209145ac585d46
Baha6161 <153951227+Baha6161@users.noreply.github.com>
2024-01-23 22:15:50 +0500

    Add files via upload

---
+++ 3.2sc.js
@@ -0,0 +1,19 @@
+// let const var
+const q = "qwerty123"
+const r = 778779
+const p = false
+
+const qXcv = String(q)
+const rXcv = String(r)
+const pXcv = String(p)
+console.log(qXcv, rXcv, pXcv)
+
+const qKli = Number (q)
+const rKli = Number (r)
+const pKli = Number (p)
+console.log(qKli, rKli, pKli)
+ 
+const qTrew = Boolean(q)
+const rTrew = Boolean(r)
+const pTrew = Boolean(p)
+console.log(qTrew, rTrew, pTrew)
